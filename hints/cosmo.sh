b6941b30f4694cddc9865bd4cffb8fddf9a1961d
Gavin Hayes <gavin@computoid.com>
2022-08-09 01:02:01 -0400

    add COSMO_MODE to control MODE for cosmo building file usage. Disable locale for now. Test against miniperl.com instead of original elf to avoid zipos failures. Failed 13 tests out of 357, 96.36% okay.

--- hints/cosmo.sh
+++ hints/cosmo.sh
@@ -22,10 +22,11 @@ libpth='none'
 locincpth=''
 usrinc="$COSMO_DUMMYDIR"
 cc="$COSMO_REPO/o/third_party/gcc/bin/x86_64-linux-musl-gcc"
-ccflags="-static -nostdlib -nostdinc -fno-pie -no-pie -mno-red-zone -fno-omit-frame-pointer -pg -mnop-mcount -mno-tls-direct-seg-refs -include $COSMO_REPO/o/cosmopolitan.h -I $COSMO_DUMMYDIR"
-ldflags="-static -nostdlib -nostdinc -fno-pie -no-pie -mno-red-zone -fno-omit-frame-pointer -pg -mnop-mcount -mno-tls-direct-seg-refs -Wl,--gc-sections -fuse-ld=bfd -Wl,-T,$COSMO_REPO/o//ape/ape.lds $COSMO_REPO/o//libc/crt/crt.o $COSMO_REPO/o//ape/ape.o"
-libs="$COSMO_REPO/o//cosmopolitan.a"
-requiredlibs="$COSMO_REPO/o//cosmopolitan.a"
+ccflags="-static -nostdlib -nostdinc -fno-pie -no-pie -mno-red-zone -fno-omit-frame-pointer -pg -mnop-mcount -mno-tls-direct-seg-refs -include $COSMO_REPO/o/cosmopolitan.h -I $COSMO_DUMMYDIR -DNO_LOCALE"
+ldflags="-static -nostdlib -nostdinc -fno-pie -no-pie -mno-red-zone -fno-omit-frame-pointer -pg -mnop-mcount -mno-tls-direct-seg-refs -Wl,--gc-sections -fuse-ld=bfd -Wl,-T,$COSMO_REPO/o/$COSMO_MODE/ape/ape.lds $COSMO_REPO/o/$COSMO_MODE/libc/crt/crt.o $COSMO_REPO/o/$COSMO_MODE/ape/ape.o"
+libs="$COSMO_REPO/o/$COSMO_MODE/cosmopolitan.a"
+requiredlibs="$COSMO_REPO/o/$COSMO_MODE/cosmopolitan.a"
 static_ext='none'
 unset COSMO_DUMMYDIR
 unset COSMO_REPO
+unset COSMO_MODE
