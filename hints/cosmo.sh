c9f3235a8cc1882927968f9495a4bf841a737304
Gavin Hayes <gavin@computoid.com>
2022-08-19 16:27:45 -0400

    Add cosmo section for miniperl in Makefile.SH, create README.cosmo with instructions, enable more C extensions

--- hints/cosmo.sh
+++ hints/cosmo.sh
@@ -31,13 +31,15 @@ uidsize='4'
 uidtype='int'
 socksizetype='socklen_t'
 d_getgrps='undef'
+d_procselfexe='undef'
 locincpth=''
 usrinc="$COSMO_REPO/libc/isystem"
 cc="$COSMO_REPO/o/third_party/gcc/bin/x86_64-linux-musl-gcc"
 ccflags="-static -nostdlib -nostdinc -fno-pie -no-pie -mno-red-zone -fno-omit-frame-pointer -fno-stack-protector -pg -mnop-mcount -mno-tls-direct-seg-refs -I $COSMO_REPO -include $COSMO_REPO/libc/integral/normalize.inc -isystem $COSMO_REPO/libc/isystem"
-ldflags="-static -nostdlib -nostdinc -fno-pie -no-pie -mno-red-zone -fno-omit-frame-pointer -fno-stack-protector -pg -mnop-mcount -mno-tls-direct-seg-refs -Wl,--gc-sections -fuse-ld=bfd -Wl,-T,$COSMO_REPO/o/$COSMO_MODE/ape/ape.lds $COSMO_REPO/o/$COSMO_MODE/libc/crt/crt.o $COSMO_REPO/o/$COSMO_MODE/ape/ape-no-modify-self.o"
+ldflags="-static -nostdlib -nostdinc -fno-pie -no-pie -mno-red-zone -fno-omit-frame-pointer -fno-stack-protector -pg -mnop-mcount -mno-tls-direct-seg-refs -Wl,--gc-sections -fuse-ld=bfd -Wl,-T,$COSMO_REPO/o/$COSMO_MODE/ape/ape.lds $COSMO_REPO/o/$COSMO_MODE/libc/crt/crt.o $COSMO_REPO/o/$COSMO_MODE/ape/ape.o"
 libs="$COSMO_REPO/o/$COSMO_MODE/cosmopolitan.a"
 requiredlibs="$COSMO_REPO/o/$COSMO_MODE/cosmopolitan.a"
-static_ext='Errno/Runtime'
+#static_ext='Errno/Runtime'
+noextensions='Fcntl IO POSIX Socket Time/HiRes re'
 unset COSMO_REPO
 unset COSMO_MODE
