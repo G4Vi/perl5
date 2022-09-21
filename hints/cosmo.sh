4ff1a7d34e7f4b4306108c68958859e7101dfcd1
Gavin Hayes <gavin@computoid.com>
2022-09-21 17:38:16 -0400

    switch to ape-no-modify-self.o, can be swapped out with COSMO_APE_LOADER

--- hints/cosmo.sh
+++ hints/cosmo.sh
@@ -3,6 +3,7 @@
 #
 
 test -d "$COSMO_REPO" || exit 1;
+${COSMO_APE_LOADER:="ape-no-modify-self.o"}
 
 usedl='undef'
 usenm='false'
@@ -20,11 +21,12 @@ locincpth=''
 usrinc="$COSMO_REPO/libc/isystem"
 cc="$COSMO_REPO/o/third_party/gcc/bin/x86_64-linux-musl-gcc"
 ccflags="-static -nostdlib -nostdinc -fno-pie -no-pie -mno-red-zone -fno-omit-frame-pointer -fno-stack-protector -pg -mnop-mcount -mno-tls-direct-seg-refs -I $COSMO_REPO -include $COSMO_REPO/libc/integral/normalize.inc -isystem $COSMO_REPO/libc/isystem"
-ldflags="-static -nostdlib -nostdinc -fno-pie -no-pie -mno-red-zone -fno-omit-frame-pointer -fno-stack-protector -pg -mnop-mcount -mno-tls-direct-seg-refs -Wl,--gc-sections -fuse-ld=bfd -Wl,-T,$COSMO_REPO/o/$COSMO_MODE/ape/public/ape.lds $COSMO_REPO/o/$COSMO_MODE/libc/crt/crt.o $COSMO_REPO/o/$COSMO_MODE/ape/ape.o"
+ldflags="-static -nostdlib -nostdinc -fno-pie -no-pie -mno-red-zone -fno-omit-frame-pointer -fno-stack-protector -pg -mnop-mcount -mno-tls-direct-seg-refs -Wl,--gc-sections -fuse-ld=bfd -Wl,-T,$COSMO_REPO/o/$COSMO_MODE/ape/public/ape.lds $COSMO_REPO/o/$COSMO_MODE/libc/crt/crt.o $COSMO_REPO/o/$COSMO_MODE/ape/$COSMO_APE_LOADER"
 libs="$COSMO_REPO/o/$COSMO_MODE/cosmopolitan.a"
 #noextensions=''
 unset COSMO_REPO
 unset COSMO_MODE
+unset COSMO_APE_LOADER
 
 # uncomment as needed for older cosmopolitan
 #d_msgctl='undef'
