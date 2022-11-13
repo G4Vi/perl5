23ce5c7e7cae6a96e9f70d1ebd7578cedafcb6f0
Gavin Hayes <gavin@computoid.com>
2022-11-13 16:51:34 -0500

    update patchlevel.h

--- patchlevel.h
+++ patchlevel.h
@@ -158,6 +158,7 @@ hunk.
 static const char * const local_patches[] = {
         NULL
         ,"G4Vi:cosmo/73ecc6c - https://github.com/G4Vi/perl5/commit/73ecc6c6640c61baa00ee91949d502f0fe991db5 various changes to support building with the Cosmopolitan Libc"
+        ,"G4Vi:cosmo/2fb034f - https://github.com/G4Vi/perl5/commit/2fb034ffb02db2b3ac09706c954704930c620c0b fix Time-HiRes Configure to work for cosmopolitan"
 #ifdef PERL_GIT_UNCOMMITTED_CHANGES
         ,"uncommitted-changes"
 #endif
